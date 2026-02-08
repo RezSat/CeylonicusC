2ecb458b89e748108b3067f30e7fda61e8d49408
RezSat <yehantest@gmail.com>
2026-02-08 22:24:48 +0530

    add is_ascii_digit

--- lexer.c
+++ lexer.c
@@ -6,3 +6,7 @@
 /* ----------------------------
    Small helpers (code points)
    ---------------------------- */
+
+static int is_ascii_digit(uint32_t cp) {
+    return cp >= (uint32_t)'0' && cp <= (uint32_t)'9';
+}
