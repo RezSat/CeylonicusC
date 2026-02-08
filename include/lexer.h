290ecf15205bdb056588e1760ec6863553e6ca22
RezSat <yehantest@gmail.com>
2026-02-08 21:55:49 +0530

    added lexer status enum

--- include/lexer.h
+++ include/lexer.h
@@ -5,6 +5,15 @@
 extern "C" {
 #endif
 
+typedef enum {
+    LEX_OK = 0,
+    LEX_EOF,
+    LEX_INVALID_UTF8,
+    LEX_ILLEGAL_CHAR,
+    LEX_UNTERMINATED_STRING,
+    LEX_EXPECTED_CHAR
+} LexerStatus;
+
 
 #ifdef __cplusplus
 }
