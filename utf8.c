c6f076d89ca269558b5f7e15b7aa74100dbf9eaf
RezSat <yehantest@gmail.com>
2026-02-08 21:05:22 +0530

    checking for ASCII

--- utf8.c
+++ utf8.c
@@ -1,5 +1,15 @@
 #include "utf8.h"
 
 Utf8Status utf8_next(const uint8_t* buf, size_t len, size_t* i, uint32_t* out_cp) {
-    
+    if (!buf || !i || !out_cp) return UTF8_INVALID;
+    if (*i >= len) return UTF8_EOF;
+
+    uint8_t b0 = buf[*i];
+
+    // ASCII: 0xxxxxxx
+    if ((b0 & 0x80u) == 0) {
+        *out_cp = (uint32_t)b0;
+        *i += 1;
+        return UTF8_OK;
+    }
 }
