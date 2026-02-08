e86aa3ddf7c86d9708f2ca527c66392620fd1c3e
RezSat <yehantest@gmail.com>
2026-02-08 22:25:45 +0530

    add is_ascii_alpha

--- lexer.c
+++ lexer.c
@@ -10,3 +10,8 @@
 static int is_ascii_digit(uint32_t cp) {
     return cp >= (uint32_t)'0' && cp <= (uint32_t)'9';
 }
+
+static int is_ascii_alpha(uint32_t cp) {
+    return (cp >= (uint32_t)'a' && cp <= (uint32_t)'z') ||
+           (cp >= (uint32_t)'A' && cp <= (uint32_t)'Z');
+}
