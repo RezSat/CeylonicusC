cf544cc0be571e6afd5bbad4ea68e48ec1b5b76c
RezSat <yehantest@gmail.com>
2026-02-08 23:19:47 +0530

    skipped whitespaces in lexer_next_token

--- lexer.c
+++ lexer.c
@@ -406,9 +406,19 @@ LexerStatus lexer_next_token(Lexer* lx, Token* out_tok) {
             token_init(out_tok, TOK_EOF, &lx->pos, &lx->pos);
             return LEX_EOF;
         }
+
         if (st != LEX_OK) {
             set_error(lx, &lx->pos, &lx->pos, 0, 0);
             return st;
         }
+        
+        // Skip spaces and tabs
+        if (cp == (uint32_t)' ' || cp == (uint32_t)'\t' || cp == (uint32_t)'\r') {
+            uint32_t consumed = 0;
+            st = advance_cp(lx, &consumed);
+            if (st != LEX_OK) return st;
+            continue;
+        }
+
     }
 }
