73a19f4cb95f8ed96209472ff59bb71d552fd79f
RezSat <yehantest@gmail.com>
2026-02-09 22:57:37 +0530

    try to code main.c

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 */
 
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "include/lexer.h"
 #include "include/keywords.h"
@@ -10,7 +11,18 @@
 
 int main() {
     FILE* fptr;
-    fptr = fopen("write.cyl", "r");
+    fptr = fopen("write.cyl", "rb");
+    fseek(fptr, 0, SEEK_END);
+    size_t size = ftell(fptr);
+    fseek(fptr, 0, SEEK_SET);
+
+    uint8_t* buffer = (uint8_t*)malloc(size);
+    fread(buffer, 1, size, fptr);
+    fclose(fptr);
     
+    // Initialize lexer
+    Lexer lx;
+    lexer_init(&lx, "write.cyl", buffer, size);
+    lexer_set_keyword_fn(&lx, lexer_default_is_keyword);
     return 0;
 }
