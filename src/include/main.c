7cb204dc744dc00793b7e1320341937254b71b9a
RezSat <yehantest@gmail.com>
2026-03-24 13:38:35 +0530

    add basic cli usage handling

--- src/include/main.c
+++ src/include/main.c
@@ -4,7 +4,16 @@
 
 #include <stdio.h>
 
-int main(void) {
-    printf("cyl driver\n");
+static void print_usage(const char *progname) {
+    fprintf(stderr, "Usage: %s <file.cyl>\n", progname);
+}
+
+int main(int argc, char **argv) {
+    if (argc != 2) {
+        print_usage(argv[0]);
+        return 1;
+    }
+
+    printf("input: %s\n", argv[1]);
     return 0;
 }
